66a7ad42626cf695d6011cc9d336c490fa17e32d
detolly <thomas@detolly.no>
2025-11-11 19:06:48 +0100

    print more

--- src/laser.c
+++ src/laser.c
@@ -17,7 +17,7 @@ int main(int argc, const char* argv[])
         points[i].x = cosf((TWO_PI / NUM_POINTS) * (float)i);
         points[i].y = sinf((TWO_PI / NUM_POINTS) * (float)i);
 
-        printf("%f,%f,%f\n", points[i].x, 0.0f, points[i].y);
+        // printf("%f,%f,%f\n", points[i].x, 0.0f, points[i].y);
     }
 
     calculate_grid_points();
@@ -27,9 +27,11 @@ int main(int argc, const char* argv[])
 
     for(unsigned i = 0; i < picture.num_points; i++) {
         point_t* p = &picture.projections[i].fixed_point;
+        point_t* p2 = &picture.projections[i].projected_point;
         angles_t* a = &picture.projections[i].fixed_angles;
         motor_instruction_pair_t* m = &picture.instructions[i];
-        printf("%f,%f,%f,%f,%f,%d,%d\n", p->x, distance_to_wall(), p->y, a->yaw, a->pitch, m->yaw.steps, m->pitch.steps);
+        printf("%f,%f,%f,%f,%f,%d,%d", p->x, distance_to_wall(), p->y, a->yaw, a->pitch, m->yaw.steps, m->pitch.steps);
+        printf(",%f,%f,%f\n", p2->x, 0.0f, p2->y);
     }
 
     picture_free(&picture);
