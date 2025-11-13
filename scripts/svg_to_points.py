eee95192155b4fa57cf9229fca3e85643e5cb294
detolly <thomas@detolly.no>
2025-11-13 13:25:41 +0100

    better scaling when converting

--- scripts/svg_to_points.py
+++ scripts/svg_to_points.py
@@ -17,7 +17,12 @@ max_point_y = max([p.imag for p in points])
 min_point_x = min([p.real for p in points])
 min_point_y = min([p.imag for p in points])
 
-new_points = [(((p.real - min_point_x) / max_point_x) * 2 - 1, ((p.imag - min_point_y) / max_point_y) * 2 - 1) for p in points]
+width = max_point_x - min_point_x
+height = max_point_y - min_point_y
+
+should_scale_y = width > height
+
+new_points = [((p.real - min_point_x) / (width if should_scale_y else height) * 2 - 1, (p.imag - min_point_y) / (width if should_scale_y else height) * 2 - 1) for p in points]
 
 # for p in new_points:
 #     print(f"{p[0]:.4f} {p[1]:.4f}")
