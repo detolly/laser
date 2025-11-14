9777ead503b1668e05c82a4a2a83ab83422bbf15
detolly <thomas@detolly.no>
2025-11-14 17:58:04 +0100

    quick and dirty

--- src/motor_test.c
+++ src/motor_test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include <config.h>
@@ -17,6 +18,15 @@ int main(int argc, const char* argv[])
     (void)argc;
     (void)argv;
 
+    config_t cfg;
+    cfg.distance_up = (float)atof(argv[1]);
+    cfg.distance_to_wall = (float)atof(argv[2]);
+    cfg.picture_size = (float)atof(argv[3]);
+    cfg.steps_per_revolution_yaw = (size_t)atoi(argv[4]);
+    cfg.steps_per_revolution_pitch = (size_t)atoi(argv[5]);
+    cfg.motor_speed = (size_t)atol(argv[6]);
+    set_config(&cfg);
+
     // for(int i = 0; i < NUM_POINTS; i++) {
     //     points[i].x = cosf((TWO_PI / NUM_POINTS) * (float)i);
     //     points[i].y = sinf((TWO_PI / NUM_POINTS) * (float)i);
@@ -24,9 +34,7 @@ int main(int argc, const char* argv[])
     // }
 
     motor_init();
-
     start_motor_thread();
-    calculate_grid_points();
 
     picture_t picture = {0};
     picture_from_points(&picture, points, NUM_POINTS);
